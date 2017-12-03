632af4921f0ca6e73d2187ce5f26553c9ff347ab
Joel May <joel@jmay.us>
2017-12-02 22:38:55 -0600

    Imported HTML from v1.0

--- HostnamePlus/Pages/Index.cshtml.cs
+++ HostnamePlus/Pages/Index.cshtml.cs
@@ -9,6 +9,38 @@ namespace HostnamePlus.Pages
 {
     public class IndexModel : PageModel
     {
+        public bool IsIPv6
+        {
+            get
+            {
+                return Request.HttpContext.Connection.RemoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6;
+            }
+        }
+
+        public String HostName
+        {
+            get
+            {
+                return "TODO HOSTNAME";
+            }
+        }
+
+        public String IP
+        {
+            get
+            {
+                return "TODO IP";
+            }
+        }
+
+        public String UserAgent
+        {
+            get
+            {
+                return "TODO USERAGENT";
+            }
+        }
+
         public void OnGet()
         {
 
