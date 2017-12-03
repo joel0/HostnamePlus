83b395fd371d12e29913de35e7d585ac0b1272f1
Joel May <joel@jmay.us>
2017-12-03 01:47:45 -0600

    Curl output

--- HostnamePlus/Controllers/IndexController.cs
+++ HostnamePlus/Controllers/IndexController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HostnamePlus.Models;
 
 namespace HostnamePlus.Controllers
 {
@@ -10,7 +11,13 @@ namespace HostnamePlus.Controllers
     {
         public IActionResult Index()
         {
-            return View(new Models.IndexModel(Request));
+            IndexModel model = new IndexModel(Request);
+            String view = "Index";
+            if (Request.Headers["User-Agent"].ToString().StartsWith("curl")) {
+                view = "Curl";
+                Response.ContentType = "text/plain";
+            }
+            return View(view, model);
         }
     }
 }
