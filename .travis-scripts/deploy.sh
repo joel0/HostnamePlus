4c1924ac708984876f6efaff892e8698c0670ea1
Joel May <jmay@iastate.edu>
2019-06-13 17:47:06 -0400

    Travis script

---
+++ .travis-scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit script if any error occurs
+set -e
+
+
+# Prepare
+dotnet restore
+
+# Build
+dotnet build -c Release
+
+# Bundle
+dotnet publish -c Release -o ./publish
+
+# Copy to production server
+ssh $deploy_user@$deploy_server sudo systemctl stop kesteral-hostname
+rsync -vrlpt \
+    --delete \
+    --delete-excluded \
+    ./HostnamePlus/publish/ $deploy_user@$deploy_server:~/www/
+ssh $deploy_user@$deploy_server sudo systemctl start kesteral-hostname
