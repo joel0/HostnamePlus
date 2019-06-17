f9a576aaf33a5e63cd7af076dfb5333ae3e79246
Joel May <jmay@iastate.edu>
2019-06-17 17:45:13 -0400

    Simple script?

--- .travis-scripts/deploy.sh
+++ .travis-scripts/deploy.sh
@@ -2,22 +2,22 @@
 
 # Exit script if any error occurs
 #set -e
+echo testing
+# cd $TRAVIS_BUILD_DIR
 
-cd $TRAVIS_BUILD_DIR
+# # Prepare
+# dotnet restore
 
-# Prepare
-dotnet restore
+# # Build
+# dotnet build -c Release
 
-# Build
-dotnet build -c Release
+# # Bundle
+# dotnet publish -c Release -o ./publish
 
-# Bundle
-dotnet publish -c Release -o ./publish
-
-# Copy to production server
-ssh $deploy_user@$deploy_server sudo systemctl stop kesteral-hostname
-rsync -vrlpt \
-    --delete \
-    --delete-excluded \
-    ./HostnamePlus/publish/ $deploy_user@$deploy_server:~/www/
-ssh $deploy_user@$deploy_server sudo systemctl start kesteral-hostname
+# # Copy to production server
+# ssh $deploy_user@$deploy_server sudo systemctl stop kesteral-hostname
+# rsync -vrlpt \
+#     --delete \
+#     --delete-excluded \
+#     ./HostnamePlus/publish/ $deploy_user@$deploy_server:~/www/
+# ssh $deploy_user@$deploy_server sudo systemctl start kesteral-hostname
