a2bbe1b45f4ca63b1056b33337b763ddd519112b
Joel May <jmay@iastate.edu>
2019-06-17 20:22:48 -0400

    Log messages for service starting

--- .travis-scripts/deploy.sh
+++ .travis-scripts/deploy.sh
@@ -13,9 +13,15 @@ set -e
 # dotnet publish -c Release -o ./publish
 
 # Copy to production server
+echo -n Stopping kesteral-hostname.service...
 ssh $deploy_user@$deploy_server sudo systemctl stop kesteral-hostname
+echo stopped
+
 rsync -vrlpt \
     --delete \
     --delete-excluded \
     HostnamePlus/publish/ $deploy_user@$deploy_server:~/www/
+
+echo -n Starting kesteral-hostname.service...
 ssh $deploy_user@$deploy_server sudo systemctl start kesteral-hostname
+echo started
