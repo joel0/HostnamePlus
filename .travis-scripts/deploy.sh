26f540383c72859c1dfbfc77ac991ae6a6eec467
Joel May <jmay@iastate.edu>
2019-06-17 20:44:54 -0400

    Use built in dotnet build

--- .travis-scripts/deploy.sh
+++ .travis-scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Exit script if any error occurs
 set -e
 
+# Bundle a release to deploy
+dotnet publish -c Release -o ./publish $TRAVIS_BUILD_DIR
+
 # Copy to production server
 echo -n Stopping $deploy_service...
 ssh $deploy_user@$deploy_server sudo systemctl stop $deploy_service
