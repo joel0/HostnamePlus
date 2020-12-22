1adbc6fdc82d93b198389d75102b1ad0ce34a8bb
Joel May <joel.may@live.com>
2020-12-21 18:04:19 -0800

    Begin testing Cloudflare resolved address in console

--- HostnamePlus/wwwroot/js/GetOtherIp.js
+++ HostnamePlus/wwwroot/js/GetOtherIp.js
@@ -5,14 +5,18 @@ req.responseType = "json";
 req.open("GET", otherIpAPIURL, true);
 req.send();
 
+var cfReq = new XMLHttpRequest();
+cfReq.onreadystatechange = handleCfReply;
+cfReq.responseType = "json";
+cfReq.open("GET", "https://hostname.jmay.xyz/api/cfIp", true);
+cfReq.send();
+
 /**
  * Handles the AJAX response
  */
 function handleReply() {
     // Falure results are discarded, as this is expected to fail of non-dual-stack connections.
-    if (this.readyState == 4
-        && this.status == 200) {
-
+    if (this.readyState == 4 && this.status == 200) {
         // This object is defined in Models/IndexModel.
         var otherIpInfo = this.response;
         var prefix = "<br /><span class='versionLabel'>" + otherIpType + ": </span>";
@@ -20,5 +24,13 @@ function handleReply() {
         var hostNameInnerHtml = prefix + otherIpInfo.hostName + "<br />";
         document.getElementById("OtherAddress").innerHTML = addressInnnerHtml;
         document.getElementById("OtherHostname").innerHTML = hostNameInnerHtml;
+    }
+}
+
+function handleCfReply() {
+    // Discard failure results
+    if (this.readyState == 4 && this.status == 200) {
+        var otherIpInfo = this.response;
+        console.log("IP as reported by Cloudflare: ", otherIpInfo)
     }
 }
